75cbb834cf4602797f6b53e577fccfcff83c6835
lunaisme <habibmaulana25.2003@gmail.com>
2024-10-16 11:27:01 +0700

    kartu anggota done

--- lib/screens/anggota/anggota_page.dart
+++ lib/screens/anggota/anggota_page.dart
@@ -9,20 +9,82 @@ class AnggotaPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Daftar anggota"),
       ),
-      body: Row(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildAnggotaCard(
+              context,
+              'https://akcdn.detik.net.id/visual/2024/07/16/spongebob-squarepants_169.png?w=400&q=90',
+              'Afif Jamhari',
+              '124220018',
+              'taken',
+              'Turu sambil Ngoding',
+            ),
+            _buildAnggotaCard(
+              context,
+              'https://64.media.tumblr.com/f6829093a6ee638026b78c1b76e5b653/tumblr_oujjbmTtDI1ue9zkjo6_500.jpg',
+              'Habib Maulana Akbar',
+              '124220022',
+              'Takenan Batin',
+              'Ngoding tapi Turu',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnggotaCard(BuildContext context, String imageUrl, String name,
+      String nim, String status, String hobby) {
+    return Card(
+      color: Colors.white,
+      child: Row(
         children: [
           Container(
-            width: MediaQuery.of(context).size.width / 2,
-            child: Image.network(
-              'https://unsplash.it/400/300',
-              fit: BoxFit.cover,
+            width: 150,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width / 2,
-            child: Image.network(
-              'https://unsplash.it/400/301',
-              fit: BoxFit.cover,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.yellow[700],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'NIM\t\t\t\t\t: $nim',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Status\t: $status',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Hobby\t: $hobby',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
